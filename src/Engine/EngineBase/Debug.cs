f79bdb85e1b2ba444d832d3a175717b121807e14
Guillaume <g.weber2002@gmail.com>
2023-12-20 21:23:34 +0100

    Log with stacktrace
    Add stacktrace on log
    add define for log stacktace
    stacktrace always log in file

--- src/Engine/EngineBase/Debug.cs
+++ src/Engine/EngineBase/Debug.cs
@@ -11,10 +11,13 @@ namespace Engine
 			m_logFilePath = EngineConfig.DebugFilePath + EngineConfig.DebugFileName;
 			using (StreamWriter sw = new StreamWriter(m_logFilePath, false))
 				sw.WriteLine("Start of the game");
+			m_stackTrace = "";
 		}
 
 		public static void LogError(string text)
 		{
+			GetStacktrace();
+			text += m_stackTrace;
 			Console.ForegroundColor = ConsoleColor.Red;
 			Console.WriteLine("[Error] " + text);
 			m_stringBuilder.AppendLine("[Error] " + text);
@@ -23,14 +26,24 @@ namespace Engine
 
 		public static void Log(string text)
 		{
+			GetStacktrace();
 			Console.WriteLine(text);
+#if GET_STACKTRACE
+			Console.WriteLine(m_stackTrace);
+			text += m_stackTrace;
+#endif
 			m_stringBuilder.AppendLine(text);
 		}
 
 		public static void LogWarning(string text)
 		{
+			GetStacktrace();
 			Console.ForegroundColor = ConsoleColor.Yellow;
 			Console.WriteLine("[Warning] " + text);
+#if GET_STACKTRACE
+			Console.WriteLine(m_stackTrace);
+#endif
+			text += m_stackTrace;
 			m_stringBuilder.AppendLine("[Warning] " + text);
 			Console.ResetColor();
 		}
@@ -44,7 +57,19 @@ namespace Engine
 			}
 		}
 
+		private static void GetStacktrace()
+		{
+			m_stackTrace = Environment.StackTrace;
+			string[] tab = m_stackTrace.Split('\n');
+			m_stackTrace = "";
+			foreach (string s in tab.Skip(3))
+			{
+				m_stackTrace += s + "\n";
+			}
+		}
+
 		private static StringBuilder m_stringBuilder = new StringBuilder();
 		private static string m_logFilePath;
+		private static string m_stackTrace;
 	}
 }
